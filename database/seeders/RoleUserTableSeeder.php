945d62f5ee6d177e7e554cca596c2cde57eb4049
heybill37 <119391419+heybill37@users.noreply.github.com>
2025-10-20 13:45:57 +0700

    update database

--- database/seeders/RoleUserTableSeeder.php
+++ database/seeders/RoleUserTableSeeder.php
@@ -22,7 +22,7 @@ class RoleUserTableSeeder extends Seeder
         ['user_id' => 4,'role_id' => 2],
         ['user_id' => 5,'role_id' => 1],
         ['user_id' => 5,'role_id' => 2],
-        ['user_id' => 6,'role_id' => 1],
+        ['user_id' => 6,'role_id' => 2],
     ]);
     }
 }
